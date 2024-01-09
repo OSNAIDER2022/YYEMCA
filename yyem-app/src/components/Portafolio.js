490a3f0bfa3c177b737f0fb00c0c276d10f3ad1a
amsterdam <amsterdam@amsterdam.amsterdam>
2024-01-08 23:41:55 -0500

    actualizaciones YYEMCA: establecimiento de grilla dinámica para la implementacion de precios de servicios. Resta por completar la grilla con el cliente

--- yyem-app/src/components/Portafolio.js
+++ yyem-app/src/components/Portafolio.js
@@ -2,51 +2,37 @@ import { useState } from 'react';
 import { Container, Col, Row, Card, Modal, Button } from 'react-bootstrap';
 import '../css/Portafolio.css'
 import datos_portafolio from '../img/portafolio/datos_portafolio.js';
-import precios from '../img/portafolio/lista_precios.js';
-
+import lista_precios from '../img/portafolio/lista_precios.js';
 
 
 function Portafolio() {
     const [modalShow, setModalShow] = useState(false);
-    const [listado, setListado] = useState(precios)
+    const [listado, setListado] = useState(lista_precios)
 
     function MydModalWithGrid(props) {
         return (
           <Modal {...props} aria-labelledby="contained-modal-title-vcenter" className="b">
             <Modal.Header closeButton >
               <Modal.Title id="contained-modal-title-vcenter">
-                Precios de viaje:
+                Esta es nuestra lista de Precios!!
               </Modal.Title>
             </Modal.Header>
             <Modal.Body>
               <Container>
-                <Row>
-                  <Col xs={6} md={4}>
-                    Destino
-                  </Col>
-                  <Col xs={6} md={4}>
-                    Hora de Salida
-                  </Col>
-                  <Col xs={6} md={4}>
-                    Valor
-                  </Col>
-                </Row>
-
                 {/* renderizado de la lista de precios */}
                 {Array.from({ length: listado.length }).map((_, idg) => (
                   <Row key={idg} className='c'>
-                    <Col xs={6} md={4}>
-                      {listado[idg].destino}
+                    <Col xs={4} md={4}>
+                      {listado[idg].item1}
                     </Col>
-                    <Col xs={6} md={4}>
-                      {listado[idg].hora_salida}
+                    <Col xs={4} md={4}>
+                      {listado[idg].item2}
                     </Col>
-                    <Col xs={6} md={4}>
-                      {listado[idg].valor}
+                    <Col xs={4} md={4}>
+                      {listado[idg].item3}
                     </Col>
                   </Row>
-                ))}                
-                
+                ))} 
               </Container>
             </Modal.Body>
             <Modal.Footer>
@@ -56,6 +42,7 @@ function Portafolio() {
         );
       }
       
+
 
     return(
         <Container>
@@ -69,12 +56,14 @@ function Portafolio() {
                 {Array.from({ length: datos_portafolio.length }).map((_, idx) => (
                     <Col key={idx} className='texto_portafolio'>
                     <Card  className='back1'>
-                        <Card.Img variant="top" src={datos_portafolio[idx].imagen} className='imagenes' onClick={() => setModalShow(true) && setListado(datos_portafolio[idx].id_lista)}/>
+                        <Card.Img variant="top" src={datos_portafolio[idx].imagen} className='imagenes' onClick={() => setModalShow(true) & setListado(datos_portafolio[idx].id_lista)}/>
                         <MydModalWithGrid show={modalShow} onHide={() => setModalShow(false)} />
                         <Card.Body>
-                        <Card.Title>{datos_portafolio[idx].titulo}</Card.Title>
+                        <Card.Title>
+                          {datos_portafolio[idx].titulo}
+                        </Card.Title>
                         <Card.Text>
-                            {datos_portafolio[idx].descripcion}
+                          {datos_portafolio[idx].descripcion}
                         </Card.Text>
                         </Card.Body>
                     </Card>
--- yyem-app/src/img/portafolio/lista_precios.js
+++ yyem-app/src/img/portafolio/lista_precios.js
@@ -1,26 +1,45 @@
-const precios = [
+const transporteIntermunicipal = [
     {
-        'destino':'Medellin-Andes',
-        'hora_salida':'12:00pm',
-        'valor':'$60.000'
+        'item1':'Destino',
+        'item2':'Hora de Salida',
+        'item3':'Valor'
     },
     {
-        'destino':'Medellin-Guatapé',
-        'hora_salida':'19:00pm',
-        'valor':'$40.000'
+        'item1':'Medellin-Andes',
+        'item2':'12:00pm',
+        'item3':'$60.000'
     },
     {
-        'destino':'Medellin-San Francisco',
-        'hora_salida':'3:00pm',
-        'valor':'$50.000'
+        'item1':'Medellin-Guatapé',
+        'item2':'19:00pm',
+        'item3':'$40.000'
+    },
+    {
+        'item1':'Medellin-San Francisco',
+        'item2':'3:00pm',
+        'item3':'$50.000'
     }
 
 ]
 
+const transporteTradicional = [
+    {
+        'item1':'Kilometraje',
+        'item2':'Valor Diurno por Km',
+        'item3':'Valor Nocturno por Km'
+    },
+    {
+        'item1':'menos de 1 Km',
+        'item2':'$5.000',
+        'item3':'$6.000'
+    }
+]
 
+export default{
+    transporteTradicional,
+    transporteIntermunicipal
+    
+    
+}
 
-
-export default(
-    precios
-)
     
